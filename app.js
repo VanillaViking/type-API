dee19a51af38011569de6f0fad768a5b57ef7a30
Ashwin Rajesh <ashwinr2k2@gmail.com>
2022-08-08 16:48:18 +1000

    minor

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 const mongoose = require('mongoose');
 
 
-mongoose.connect(processs.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let userSchema = new mongoose.Schema({
   discordId: String,
