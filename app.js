08c815f04ec20ea14ef0f7116e8377ce093c9c74
Ashwin Rajesh <ashwinr2k2@gmail.com>
2022-08-08 16:31:16 +1000

    added rank

--- app.js
+++ app.js
@@ -37,10 +37,38 @@ app.get('/:userId/stats', async function(req, res) {
   } else {
     user = await User.findById(doc._id);
     let avg = user.tests.reduce((total, num) => {return total + num}, 0) / user.tests.length;
-    res.json({"username": user.username, "average": avg, "tests": user.tests.length})
+    let rank = getRank(avg);
+    res.json({"username": user.username, "average": avg, "tests": user.tests.length, rank: rank})
   }
 })
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port 3000`)
 })
+
+
+function getRank(avg) {
+  let rank = 'Undetermined';
+  if (avg < 50) {
+    rank = "Novice"
+  } else if (avg < 60) {
+    rank = "Iron"
+  } else if (avg < 70) {
+    rank = "Bronze"
+  } else if (avg < 80) {
+    rank = "Silver"
+  } else if (avg < 90) {
+    rank = "Gold"
+  } else if (avg < 100) {
+    rank = "Diamond" 
+  } else if (avg < 110) {
+    rank = "Platinum"
+  } else if (avg < 120) {
+    rank = "Demon"
+  } else if (avg >= 120) {
+    rank = "God"
+  }
+  return rank;
+
+}
