ff9ffe80f8c066b55ab4c7648a55dd4a2549e63c
Ashwin Rajesh <ashwinr2k2@gmail.com>
2022-08-09 20:45:34 +1000

    leaderboards

--- app.js
+++ app.js
@@ -56,11 +56,12 @@ app.get('/:userId/stats', async function(req, res) {
 
     //let averages = getAverages(user.tests);
     //let rank = getRank(averages[0]);
+    console.log(tests)
     if (!(tests === [])) {
       tests[0].rank = getRank(tests[0].averageWpm)
-      res.json(null)
-    } else {
       res.json(tests[0]) 
+    } else {
+      res.json(null)
     }
 })
 
